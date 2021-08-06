042f9a250fc1d79240c0cbd0e4c8b75f7514080c
TdotShare <52649379+TdotShare@users.noreply.github.com>
2021-08-06 23:03:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pythainlp.tokenize import word_tokenize
+
+from pydantic import BaseModel
+
+
+class ResponseData(BaseModel):
+    text: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello TdotDev" , "api" : "FastAPI - Python"}
+
+
+@app.post("/api/word_tokenize")
+def tokenize(data: ResponseData):
+    try:
+        tokens = word_tokenize(
+            text=data.text, engine="newmm", keep_whitespace=False)
+        return {'status': 'success',
+                'bypass': True,
+                'data': unique(tokens)}
+    except Exception as e:
+        return {'status': 'error',
+                'bypass': False,
+                'message': 'Exception: ' + str(e)}
+
+
+def unique(item):
+    unique_list = []
+    for x in item:
+        if x not in unique_list:
+            unique_list.append(x)
+
+    return unique_list
+     
